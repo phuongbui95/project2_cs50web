d2eb872c6c41a7c676cd151482d8af6d14001777
PB Github <phuong.buitan95@gmail.com>
2023-04-04 15:23:52 +0700

    First touch

---
+++ commerce/auctions/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Listing():
+    pass
+
+class Bid():
+    pass
+
+class Comment():
+    pass
